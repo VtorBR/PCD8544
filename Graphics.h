ac9cafee199234d214f700c2c63e568ea20e5909
Vítor Fernandes <vitor_resende@ymail.com>
2018-04-28 16:21:18 -0300

    Improved Graphics API

--- Graphics.h
+++ Graphics.h
@@ -7,6 +7,7 @@ namespace PCD8544
 {
 	namespace Graphics
 	{
-		void Write(const std::uint8_t line, const std::uint8_t x, const std::string & text);
+		void DrawGlyph(const std::uint8_t line, const std::uint8_t x, const std::uint8_t & character);
+		void Write(std::uint8_t line, std::uint8_t x, const std::string & text);
 	}
 }
