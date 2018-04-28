997ecc5bf0b12ea36012187b0f98eeebe5d3ce5c
Vítor Fernandes <vitor_resende@ymail.com>
2018-04-28 12:42:49 -0300

    Added the instruction constants

--- Driver.h
+++ Driver.h
@@ -9,5 +9,21 @@ namespace PCD8544
 		void Initialize(const int SerialDataInput, const int SerialClockInput, const int ModeSelect, const int ChipEnable, const int ResetInput);
 		void Execute(const std::uint8_t instruction);
 		void WriteData(const std::uint8_t data);
+
+		namespace Instruction
+		{
+			constexpr std::uint8_t FunctionSet = 0x20;
+
+			constexpr std::uint8_t DisplayControl = 0x08;
+			constexpr std::uint8_t SetYAddress = 0x40;
+			constexpr std::uint8_t SetXAddress = 0x80;
+
+			namespace Extended
+			{
+				constexpr std::uint8_t TemperatureControl = 0x04;
+				constexpr std::uint8_t BiasSystem = 0x10;
+				constexpr std::uint8_t SetVop = 0x80;
+			}
+		};
 	}
 }
