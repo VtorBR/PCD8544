e6225d0541a4c3aa21587456a436213ef79cf353
Vítor Fernandes <vitor_resende@ymail.com>
2018-04-28 14:26:30 -0300

    Implemented display refresh

--- Display.cpp
+++ Display.cpp
@@ -6,6 +6,8 @@ namespace PCD8544
 {
 	namespace Display
 	{
+		static FrameBuffer buffer = { 0 };
+
 		void Initialize(const int SerialDataInput, const int SerialClockInput, const int ModeSelect, const int ChipEnable, const int ResetInput, const std::uint8_t Contrast)
 		{
 			assert(Contrast < 0x80);
@@ -17,6 +19,19 @@ namespace PCD8544
 			Driver::Execute(Driver::Instruction::Extended::SetVop | Contrast);
 			Driver::Execute(Driver::Instruction::FunctionSet | Driver::InstructionSet::Basic);
 			Driver::Execute(Driver::Instruction::DisplayControl | Driver::DisplayConfiguration::Normal);
+		}
+
+		void Refresh()
+		{
+			Driver::Execute(Driver::Instruction::SetYAddress);
+			Driver::Execute(Driver::Instruction::SetXAddress);
+
+			for (const std::uint8_t data : buffer)
+			{
+				Driver::WriteData(data);
+			}
+
+			Driver::Execute(Driver::Instruction::SetYAddress);
 		}
 	}
 }
